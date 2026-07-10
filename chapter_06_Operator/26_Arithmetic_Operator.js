806666e887ed18ba8b1cf0c93d2e490e1ede0aaf
vermav27 <verma.vineet27@gmail.com>
2026-07-10 13:22:26 +0530

    Add operator chapter and update README

---
+++ chapter_06_Operator/26_Arithmetic_Operator.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 2;
+
+//Arithmetic Operator
+let ans1 = x + y;
+let ans2 = x - y;
+let ans3 = x * y;
+let ans4 = x / y;
+
+console.log(ans1);
+console.log(ans2);
+console.log(ans3);
+console.log(ans4);
+
---
+++ chapter_06_Operator/28_Exponential_Operator.js
@@ -0,0 +1,8 @@
+//Exponential Iperator
+console.log(2 ** 3); // It means 2 to the power 3
+
+let x = 5;
+let y = 4;
+console.log(5 ** 4); // It means 5 to the power 4
+
+
