79bb7f4b4be382ad79befe51f059fb0352c09294
Vineet Verma <vineetverma@Vineets-MacBook-Air.local>
2026-07-14 00:06:00 +0530

    ifelse

---
+++ chapter_07_if_else/38_if_else.js
@@ -0,0 +1,7 @@
+let age = 20;
+
+if(age > 18){
+    console.log("Allowed to vote");
+}else{
+    console.log("Not allowed to vote");
+}
---
+++ chapter_07_if_else/39_if_elseif_else.js
@@ -0,0 +1,9 @@
+let x = 15;
+
+if(x < 10){
+    console.log("Less than 10");
+}else if(x < 20){
+    console.log("Less than 20");
+}else{
+    console.log("Equal to 20 or Greater than 20");
+}
