52502d9946af253f5fe5241a362dfe7fda95b1b7
vermav27 <verma.vineet27@gmail.com>
2026-09-12 16:02:38 +0530

    comment added

---
+++ chapter_22_Abstraction/157_Interface_Misc.ts
@@ -0,0 +1,10 @@
+interface dictionary {
+    [key: string]: string; // array type
+}
+
+const english: dictionary = {
+    foo: "hello",
+    hola: "mola"
+}
+
+// creating dictionary type of interface as this example has array
